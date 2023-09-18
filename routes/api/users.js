2d111980e03d1845c56bc98fa8487eb44dbd7278
Deeyomz <diomedesjrco@gmail.com>
2023-09-19 02:29:12 +0800

    User Authentication and Login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,9 @@ const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
@@ -47,11 +50,26 @@ router.post(
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
-      await user.save(0);
+      await user.save();
 
       // Return jsonwebtoken
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
