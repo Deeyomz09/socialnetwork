524efff7368b8fb25ea3c937827ce758cba9746f
Deeyomz <diomedesjrco@gmail.com>
2023-10-05 02:41:37 +0800

    Fix routing and added Store

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 //Redux
 import { Provider } from 'react-redux';
@@ -14,11 +16,19 @@ import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
   useEffect(() => {
+    // check for token in LS when app first runs
     if (localStorage.token) {
-      setAuthToken(localStorage);
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
     }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser());
 
-    store.dispatch(loadUser);
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
   }, []);
 
   return (
@@ -38,6 +48,10 @@ const App = () => {
           <Route
             path="login"
             element={<Login />}
+          />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
           />
         </Routes>
       </Router>
