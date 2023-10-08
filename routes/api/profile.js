c688bd425238176e3003ac22c04e415086725737
Deeyomz <diomedesjrco@gmail.com>
2023-10-08 17:12:58 +0800

    fix deleting post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 const request = require('request');
 const config = require('config');
+const Post = require('../../models/Post');
 // @route   GET api/profile/me
 // @desc    Get current user profile
 // @access  Private
@@ -148,7 +149,9 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    // @todo remove user posts
+    //Remove user post
+
+    await Post.deleteMany({ user: req.user.id });
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     //Remove User
